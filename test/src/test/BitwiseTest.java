8dec4baf7f6723ce4972b8f910115e8fb9f63b62
Ali-PC\Ali <ali.nasim01@gmail.com>
2020-03-05 22:26:57 +0530

    test

---
+++ test/src/test/BitwiseTest.java
@@ -0,0 +1,11 @@
+package test;
+
+public class BitwiseTest {
+
+	public static void main(String[] args) {
+		System.out.println(1^3);// 0001 ^ 0011 -> 0010 ->2
+		System.out.println(1^2);
+
+	}
+
+}
--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,22 +1,35 @@
 package test;
 
-class Abc {
-	
-	void test(String st) {
-		System.out.println("hello");
-	}
-	
-	void test(Object ob) {
-		System.out.println("goooo");
-	}
-}
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Test {
+	
+	class Abc {
+		
+		void test(String st) {
+			System.out.println("hello");
+		}
+		
+		void test(Object ob) {
+			System.out.println("goooo");
+		}
+	}
 
 
 	public static void main(String[] args) {
+		
+		Set<Integer> tree = new TreeSet<>((a, b)->a.compare(b, a));
+		tree.add(10);
+		tree.add(4);
+		tree.add(8);
+		tree.add(15);
+		for(int t: tree) {
+			System.out.print(t+" ");
+		}
+		System.out.println("*************");
 
-		Abc test = new Abc();
+		Test.Abc test = new Test().new Abc();
 		
 		System.out.println("******1****##");
 		Runtime runtime = Runtime.getRuntime();
